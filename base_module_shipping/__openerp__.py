3911837150c35a4aba54ed2958e78685dcd89c4e
Planet Odoo <info@planet-odoo.com>
2015-11-16 13:47:13 +0000

    cost

--- base_module_shipping/__openerp__.py
+++ base_module_shipping/__openerp__.py
@@ -20,18 +20,20 @@
 
 
 {
-    'name': 'Shipping Service Integration',
+    'name': 'Shipping Base Module',
     'version': '1.0',
     'category': 'Generic Modules/Warehouse Management',
     'description': """
      Odoo Integration with USPS, UPS and Fedex
     """,
-    "website" : "www.teckzilla.net",
-    'author': 'Teckzilla Software Solutions',
+    "website" : "http://planet-odoo.com/",
+    'author': 'Planet Odoo',
     'depends': ['sale','stock','delivery','product','sale_stock'],
     'css': [
         'static/src/css/rotate_label.css',
         ],
+	"price": "50",
+    "currency": "EUR",
     "demo" : [],
     "data": [
         "security/shipping_security.xml",
